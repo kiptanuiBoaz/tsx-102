e7995794cbb3ee24156ef9ae933c4835bd0cb570
kiptanui boaz <boaserem022@gmail.com>
2022-12-16 14:10:28 +0300

    clean up files

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const name: string = "App"; 
+  const age : number = 23;
+  const isMarried : boolean = false;
+
+  const getName = (name: string):number =>{
+    if(name==="App") {
+      return 29;
+    }else{
+      return 0;
+    }
+  }
+
+
   return (
     <div className="App">
-   <p> Testing</p>
+     
+   <p> {name}</p>
+   <p>{age}</p>
+   <p>{isMarried? "Married" : "Not Married"}</p>
     </div>
   );
 }
