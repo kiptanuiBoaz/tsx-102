02891631c143002a29585ebeebacd8ccc8bd3387
kiptanui boaz <boaserem022@gmail.com>
2022-12-17 10:27:14 +0300

    enums

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 
 //making the props optional
 interface Props {
@@ -9,14 +9,28 @@ interface Props {
 
 }
 
-export const Person :FC  <Props> = ({name, email, age}) => {
+export const Person: FC<Props> = ({ name, email, age }) => {
 
+    const [country, setCountry] = useState<string | null>("");
 
+    enum HairColor{
+        Blonde= "Your hair is blode, good for you!",
+        Brown = "Cool hair color",
+        Pink = "pink ! nice one"
+    }
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setCountry(event.target.value)
+
+    }
     return (
         <div>
             <p>{name}</p>
             <p>{email}</p>
             <p>{age}</p>
+            <input placeholder='Write down your counrty' onChange={handleChange} />
+            <p>{country}</p>
+            {HairColor.Blonde}
         </div>
 
     )
